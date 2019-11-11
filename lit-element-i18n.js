37975cc0ffab20d41c6a80b1721f92527418610f
wakhea <wassim.khene@polymtl.ca>
2019-11-11 17:30:54 -0500

    added language on first update

--- lit-element-i18n.js
+++ lit-element-i18n.js
@@ -3,25 +3,26 @@ import backend from 'i18next-xhr-backend'
 
 export const i18nMixin = baseClass => class extends baseClass {
 
-    firstUpdated() {
+    firstUpdated(lng) {
         i18next.on('initialized', options => {
             this.requestUpdate()
         })
         i18next.on('languageChanged', options => {
             this.requestUpdate()
         })
-
-        i18next.use(backend);
-        i18next.init({
-            lng: 'en',
-            debug: true,
-            defaultNS: 'app',
-            ns: ['app'],
-            fallbackLng: 'en',
-            backend: {
-                loadPath: this.languageResources || '/assets/locales/{{lng}}/{{ns}}.json'
-            }
-        })
+        if (!i18next.isInitialized) {
+            i18next.use(backend);
+            i18next.init({
+                lng: lng,
+                debug: true,
+                defaultNS: 'app',
+                ns: ['app'],
+                fallbackLng: 'en',
+                backend: {
+                    loadPath: this.languageResources || '/assets/locales/{{lng}}/{{ns}}.json'
+                }
+            })
+        }
 
         super.firstUpdated && super.firstUpdated()
     }
